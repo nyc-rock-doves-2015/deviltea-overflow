cb2bde2d34f435f88ee6e30fed46e285399f04e5
bicyclethief <bicyclethief@gmail.com>
2015-04-30 19:05:57 -0400

    Seed db with a question and answer to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 
-User.create(name: 'bob', password: '123')
+10.times do
+  User.create(name: TubularFaker.name, password: '123')
+end
+
+Question.create(title: TubularFaker.company, content: TubularFaker.lingo, user: User.find(User.pluck(:id).sample))
+
+Answer.create(content: TubularFaker.city, user: User.find(User.pluck(:id).sample))
+
