e3adfc7a2412bbd346a4951be5c0a140dc2fc694
Dan Asselin <dasselin86@gmail.com>
2015-05-01 14:52:36 -0400

    Implement #session_params action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:id])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path
-    else
-      redirect_to login_path
-    end
+    # user = User.find_by(session_params)
+    # if user && user.authenticate(params[:password])
+    #   session[:user_id] = user.id
+    #   redirect_to root_path
+    # else
+    #   redirect_to login_path
+    # end
   end
 
   def destroy
     #set session id to nil
     #redirect to homepage
+  end
+
+  def session_params
+    params.require(:session).permit(:name, :password)
   end
 end
