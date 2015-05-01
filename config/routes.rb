ed6c9b6b50a5396f52fdad0acb962f2765014d42
Dan Asselin <dasselin86@gmail.com>
2015-05-01 15:39:10 -0400

    Add root route to config/routes.rb
    Root route points to questions#index, which does not yet exist

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
     resources :answers, only: [:create]
   end
 
+  root to: 'questions#index'
+
   resources :comments
   resource :session, only: [:new, :create, :destroy]
+
+  get '/logout', to: 'sessions#destroy'
+
+
 end
