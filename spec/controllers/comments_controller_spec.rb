5828f750f6ddc5747f87658dcc402f9939226f8b
Lucian Kahn <luciankahn@gmail.com>
2015-05-01 14:50:37 -0400

    create doubles for user1, user2, question, answer in comment_controller_spec

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :controller do
 
+  let(:duck_user1) { double("User", name: "Daffy", password_digest: "123")}
+  let(:duck_user2) { double("User", name: "Plucky", password_digest: "123")}
+  let(:duck_question) { double("Question", title: "Does it quack?",
+                                           content: "I have a duck I need to assess.",
+                                           user: :duck_user1)}
+  let(:duck_answer) { double("Answer", content: "It quacks.",
+                                       user: :duck_user2)}
 
   it 'creates a new comment that knows if it is a response to a Question or an Answer'
 
