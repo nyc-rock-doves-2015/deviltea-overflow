42928ccc6e7d0446b4f66a79bf4c14c3f6eeb5f1
Lucian Kahn <luciankahn@gmail.com>
2015-05-01 13:48:35 -0400

    add commentable_type to comments_controller and _comment_form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,8 @@ class CommentsController < ApplicationController
   def create
     Comment.create(content: params[:content],
                    user_id: params[:user],
-                   commentable_id: params[:commentable_id])
+                   commentable_id: params[:commentable_id],
+                   commentable_type: params[:commentable_type])
   end
 
 end
