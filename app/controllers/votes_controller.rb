d44541832f9bb8a6d928d73c14b26691a529a611
Lucian Kahn <luciankahn@gmail.com>
2015-05-01 19:58:49 -0400

    detailed pseudocode for vote creation in VotesController and votes.js

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,12 @@
 class VotesController < ApplicationController
   def create
-    # is_up will be determined by the html class of the button that was clicked on, so
-      # it needs to be passed by the ajax call.
-    # votable_id will be the id of the question or answer I'm voting on.
-    # votable_type will be "Question" or "Answer"
+    # Sent from JavaScript:
+      # is_up will be determined by the html class of the button that was clicked on, so
+        # it needs to be passed by the ajax call.
+      # votable_id will be the id of the question or answer I'm voting on.
+      # votable_type will be "Question" or "Answer"
+    # If it's an upvote, add 1 to the current question/answer/comment's SCORE property.
+    # Else, subtract 1 from the current question/answer/comment's SCORE property.
 
     # Vote.create(is_up: , user_id: session[:user_id], votable_id: , votable_type: )
   end
