4f29b008417d882eb7dea3687a03ba0c54f46f58
Dan Asselin <dasselin86@gmail.com>
2015-05-01 13:39:03 -0400

    Add skeletons for three sessions actions: new, create and destroy

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #create a new session
+    # redirect to homepage
+  end
+
+  def destroy
+    #set session id to nil
+    #redirect to homepage
+  end
+end
