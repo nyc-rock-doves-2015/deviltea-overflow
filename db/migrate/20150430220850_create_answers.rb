6cb310b64e3d67e5eabf1d5ff8ca3ea00b5e1b31
Vinh Bui <bicyclethief@gmail.com>
2015-05-01 14:42:24 -0400

    Add question_id foreign key to table answers

--- db/migrate/20150430220850_create_answers.rb
+++ db/migrate/20150430220850_create_answers.rb
@@ -3,6 +3,7 @@ class CreateAnswers < ActiveRecord::Migration
     create_table :answers do |t|
       t.text :content, null: false
       t.references :user, null: false
+      t.references :question, null: false
       t.timestamps
     end
   end
