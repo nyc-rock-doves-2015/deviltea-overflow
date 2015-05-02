bb87d52f5edd942d20defe6c67b71c43de6e2231
Lucian Kahn <luciankahn@gmail.com>
2015-05-02 14:13:31 -0400

    fix params bug by changing symbol to string in form

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -10,8 +10,14 @@ class AnswersController < ApplicationController
   end
 
   def mark_as_best
-    question = params[:question]
-    answer = params[:answer]
+    puts "HERE ARE THE PARAMSSSSSS:"
+    puts params
+    puts "XXXXXXXXX"
+    question = params["question"]
+    answer = params["answer"]
+    puts "HERE IS WHAT IS IN ANSWER"
+    puts answer
+    puts "THERE IT WAS"
     question.best_answer = answer.id
     redirect_to question_path(question)
   end
