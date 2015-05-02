c2fe39b8bf9cb963cdf81292cde79b9eb565a2a9
suprfrye <tara.c.frye@gmail.com>
2015-05-01 20:38:28 -0400

    Add validations to Comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :votes, as: :votable
+
+  validates :content, :user_id, :commentable_id, presence: true
 end
