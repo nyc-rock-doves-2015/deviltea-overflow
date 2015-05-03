3edb7e2f5c0f23651ee83f754267183029963f3d
Vinh Bui <bicyclethief@gmail.com>
2015-05-02 20:42:06 -0400

    New schema as a result of unique index on table votes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
- # encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20150502170152) do
     t.datetime "updated_at"
   end
 
+  add_index "votes", ["user_id", "votable_id", "votable_type"], name: "index_votes_on_user_id_and_votable_id_and_votable_type", unique: true, using: :btree
   add_index "votes", ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id", using: :btree
 
 end
