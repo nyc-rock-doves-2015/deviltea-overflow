1fabfca5a7a964b926ea6b66b843a3fa30e0971f
Vinh Bui <bicyclethief@gmail.com>
2015-05-04 09:36:40 -0400

    Fix broken answer creation

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -20,7 +20,8 @@ class AnswersController < ApplicationController
   private
 
   def answer_params
-    params[:comment][:user_id] = session[:user_id]
-    params.require(:comment).permit(:is_up, :user_id, :votable_id, :votable_type)
+    params[:answer][:user_id] = session[:user_id]
+    params[:answer][:question_id] = params[:question_id]
+    params.require(:answer).permit(:content, :user_id, :question_id)
   end
 end
